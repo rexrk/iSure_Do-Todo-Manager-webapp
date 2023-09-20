e1bd0d322be051879b3b9a4922377568d6681872
RexRK <kraman938@gmail.com>
2023-09-20 20:24:16 +0530

    Project Initialized

---
+++ src/test/java/com/raman/springboottodoapp/SpringBootTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raman.springboottodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTodoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
